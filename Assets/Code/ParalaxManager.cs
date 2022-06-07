c5e7e87504d1b0d21702f242753f28fb232bb17a
Максим Высоцкий <visotskiy.m@gmail.com>
2022-06-07 13:16:49 +0300

    Platform + middleground

--- Assets/Code/ParalaxManager.cs
+++ Assets/Code/ParalaxManager.cs
@@ -2,23 +2,33 @@
 
 public class ParalaxManager
 {
-    private const float Coef = 0.3f;
+    private const float backGroundCoef = 0.3f;
+    private const float middleGroundCoef = 0.5f;
 
     private Camera _camera;
     private Transform _backTransform;
+    private Transform _middleGroundTransform;
+
     private Vector3 _backStartPosition;
+    private Vector3 _middleGroundStartPosition;
+
     private Vector3 _cameraStartPosition;
-    public ParalaxManager(Camera camera, Transform backTransform)
+
+    public ParalaxManager(Camera camera, Transform backTransform, Transform middleGround)
     {
         _camera = camera;
         _backTransform = backTransform;
+        _middleGroundTransform = middleGround;
+
         _backStartPosition = backTransform.position;
+        _middleGroundStartPosition = middleGround.position;
         _cameraStartPosition = camera.transform.position;
     }
 
     public void Update()
     {
-        _backTransform.position = _backStartPosition + (_camera.transform.position - _cameraStartPosition) * Coef;
+        _backTransform.position = _backStartPosition + (_camera.transform.position - _cameraStartPosition) * backGroundCoef;
+        _middleGroundTransform.position = _middleGroundStartPosition + (_camera.transform.position - _cameraStartPosition) * middleGroundCoef;
     }
     
 }
--- Assets/Code/Root.cs
+++ Assets/Code/Root.cs
@@ -4,12 +4,13 @@ public class Root : MonoBehaviour
 {
     [SerializeField] private Camera _camera;
     [SerializeField] SpriteRenderer _background;
+    [SerializeField] SpriteRenderer _middleground;
 
     private ParalaxManager _paralaxManager;
 
     private void Start()
     {
-        _paralaxManager = new ParalaxManager(_camera, _background.transform);
+        _paralaxManager = new ParalaxManager(_camera, _background.transform, _middleground.transform);
     }
 
     private void Update()
